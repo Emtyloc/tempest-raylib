bf0971f0d50077b17d59cc5e537593129d8c77d0
Emtyloc <mitoro1@miuandes.cl>
2024-09-28 05:18:53 -0300

    feat: finish world design

--- main.py
+++ main.py
@@ -1,23 +1,61 @@
 from pyray import *
 from worlds import *
 
-def draw_hole_points(level_data: LevelData):
-    for x, y in zip(level_data.x, level_data.y):
-        draw_circle(int(x), int(y), 1, RED)
+#SETTINGS
+SCREEN_WIDTH: int = 600
+SCREEN_HEIGHT: int = 800
+TARGET_FPS = 60
+screen_center = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+
+
+def draw_world(level_data: LevelData):
+    for i in range(16):
+        # Draw main figure points
+        pers_scale = 0.12
+        custom_blue = Color(16, 14, 100, 255)
+        border_blue = Color(27, 38, 169, 255)
+        draw_circle(int(level_data.x[i]), int(level_data.y[i]), 2, border_blue)
+        # draw_circle(int(screen_center.x + (level_data.x[i] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i] - screen_center.y + level_data.y3d)*pers_scale), 1, SKYBLUE)
+        org_vec = Vector2(level_data.x[i],level_data.y[i])
+        des_vec = Vector2(int(screen_center.x + (level_data.x[i] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i] - screen_center.y + level_data.y3d)*pers_scale))
+        draw_line_ex(org_vec, des_vec, 2, custom_blue)
+        # draw_line_ex(org_vec, des_vec, 2, SKYBLUE)
+        # draw_line(level_data.x[i],level_data.y[i], int(screen_center.x + (level_data.x[i] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i] - screen_center.y + level_data.y3d)*pers_scale), SKYBLUE)
+        if i > 0:
+            # draw world figure
+            org_vec = Vector2(level_data.x[i], level_data.y[i])
+            des_vec = Vector2(level_data.x[i-1], level_data.y[i-1])
+            draw_line_ex(org_vec, des_vec, 2, border_blue)
+            # draw_line(level_data.x[i],level_data.y[i], level_data.x[i-1], level_data.y[i-1], BLUE)
+            org_vec = Vector2(int(screen_center.x + (level_data.x[i] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i] - screen_center.y + level_data.y3d)*pers_scale))
+            des_vec = Vector2(int(screen_center.x + (level_data.x[i-1] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i-1] - screen_center.y + level_data.y3d)*pers_scale))
+            draw_line_ex(org_vec, des_vec, 1, border_blue)
+            # draw_line(int(screen_center.x + (level_data.x[i] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i] - screen_center.y + level_data.y3d)*pers_scale), int(screen_center.x + (level_data.x[i-1] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[i-1] - screen_center.y + level_data.y3d)*pers_scale), BLUE)
+            # draw world perspective
+            # draw perspective lines
+
+    else:
+        if not level_data.open_state:
+            org_vec = Vector2(level_data.x[0],level_data.y[0])
+            des_vec = Vector2(level_data.x[-1], level_data.y[-1])
+            draw_line_ex(org_vec, des_vec, 2, border_blue)
+            # draw_line(level_data.x[0],level_data.y[0], level_data.x[-1], level_data.y[-1], BLUE)
+            org_vec = Vector2(int(screen_center.x + (level_data.x[0] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[0] - screen_center.y + level_data.y3d)*pers_scale))
+            des_vec = Vector2(int(screen_center.x + (level_data.x[-1] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[-1] - screen_center.y + level_data.y3d)*pers_scale))
+            draw_line_ex(org_vec, des_vec, 1, border_blue)
+            # draw_line(int(screen_center.x + (level_data.x[0] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[0] - screen_center.y + level_data.y3d)*pers_scale), int(screen_center.x + (level_data.x[-1] - screen_center.x)*pers_scale), int(screen_center.y + level_data.y3d + (level_data.y[-1] - screen_center.y + level_data.y3d)*pers_scale), BLUE)
+
 
 
 def main():
 
-    # SCREEN SETTINGS
-    SCREEN_WIDTH: int = 600
-    SCREEN_HEIGHT: int = 800
-    TARGET_FPS = 60
+    
     init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Tempest Raylib")
     set_target_fps(TARGET_FPS)
 
     # CAMERA SETTINGS
-    screen_center = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
-
+   
     camera = Camera2D()
     camera.target = screen_center  # WHERE THE CAMARA IS LOCATED INSIDE THE GAME
     camera.offset = (
@@ -27,7 +65,7 @@ def main():
     camera.zoom = 1
 
     world_idx = 0
-    worlds = [circle_world, square_world, plus_world, peanut_world, cross_world, triangle_world, clover_world, vee_world, steps_world, u_shape_world]
+    worlds = [circle_world, square_world, plus_world, peanut_world, cross_world, triangle_world, clover_world, vee_world, steps_world, u_shape_world, line_world, heart_world, star_world, w_shape_world, broken_v_world, infinity_world]
     # Main game loop
     while not window_should_close():
         # LOOP SETTINGS BEFORE START THE RENDERING - BEGIN_DRAWING
@@ -49,7 +87,7 @@ def main():
         # camera.rotation+=0.1
         begin_mode_2d(camera)
         draw_circle(int(screen_center.x), int(screen_center.y), 1, GREEN)
-        draw_hole_points(worlds[world_idx])
+        draw_world(worlds[world_idx])
         end_mode_2d()
         end_drawing()
 
--- world_gen.py
+++ world_gen.py
@@ -8,7 +8,7 @@ screen_center = Vector2(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
 
 def circle():
-    radius_px = 200
+    radius_px = 210
     center = screen_center
 
     xs = []
@@ -148,20 +148,110 @@ def u_shape():
     print(xs)
     print(ys)
 
+def line():
+    xp = -7.5
+    xs = []
+    ys = []
+    for _ in range(16):
+        x = int(-xp*40) + screen_center.x
+        y = 160 + screen_center.y
+        xp+=1
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
+def heart():
+    stepsx = [2.0, 5.0, 6.0, 6.0, 5.0, 3.0, 0.0,-3.0,-5.0,-6.0,-6.0,-5.0,-2.0,-0.5, 0.0, 0.5]
+    stepsy = [6.0, 5.7, 2.0,-2.0,-5.0,-7.0,-8.0,-7.0,-5.0,-2.0, 2.0, 5.7, 6.0, 3.0,-1.0, 3.0]
+    
+    xs = []
+    ys = []
+    for a in range(16):
+        x = int(stepsx[a]*30) + screen_center.x
+        y = int(-stepsy[a]*30) + screen_center.y
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
+def star():
+    stepsx = [1.5,2.5,2.0, 2.5, 1.5, 1.0, 0.0,-1.0,-1.5,-2.5,-2.0,-2.5,-1.5,-1.0,0.0,1.0]
+    stepsy = [1.3,1.0,0.0,-1.0,-1.3,-2.2,-1.7,-2.2,-1.3,-1.0, 0.0, 1.0, 1.3, 2.2,1.7,2.2]
+
+    xs = []
+    ys = []
+    for a in range(16):
+        x = int(-stepsx[a]*80) + screen_center.x
+        y = int(stepsy[a]*100) + screen_center.y
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
+def w_shape():
+    stepsx = [ 8.0, 7.0, 6.7, 6.1, 4.7, 2.7, 1.5, 0.6,-0.6,-1.5,-2.7,-4.7,-6.1,-6.7,-7.0,-8.0]
+    stepsy = [-3.0,-1.0, 1.5, 3.7, 5.2, 5.2, 4.0, 2.0, 2.0, 4.0, 5.2, 5.2, 3.7, 1.5,-1.0,-3.0]
+
+    xs = []
+    ys = []
+    for a in range(16):
+        x = int(stepsx[a]*35) + screen_center.x
+        y = int(stepsy[a]*35) + screen_center.y
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
+
+def broken_v():
+    stepsx = [ -8.0, -7.5, -7.0, -6.5, -4.0, -2.8, -2.1, -1.1, 1.0, 2.5, 3.7, 4.5, 5.0, 5.5, 6.5,8.0]
+    stepsy = [ 8.0, 5.4, 3.0, 0.2, 0.7,-1.5,-4.0,-5.5,-5.0,-6.5,-4.0,-2.0, 1.0, 3.0, 5.0, 7.0]
+
+    xs = []
+    ys = []
+    for a in range(16):
+        x = int(-stepsx[a]*35) + screen_center.x
+        y = int(-stepsy[a]*30) + screen_center.y
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
+def infinity():
+    stepsx = [0.0,-1.0,-3.0,-5.0,-6.0,-5.0,-3.0,-1.0, 0.0, 1.0, 3.0, 5.0, 6.0, 5.0, 3.0, 1.0]
+    stepsy = [0.0, 2.0, 3.0, 2.0, 0.0,-2.0,-3.0,-2.0, 0.0, 2.0, 3.0, 2.0, 0.0,-2.0,-3.0,-2.0]
+
+    xs = []
+    ys = []
+    for a in range(16):
+        x = int(-stepsx[a]*48) + screen_center.x
+        y = int(-stepsy[a]*55) + screen_center.y
+        xs.append(x)
+        ys.append(y)
+    print(xs)
+    print(ys)
+
 
 
 def main():
-    # circle_hole()
+    # circle()
     # square()
     # cross()
     # peanut()
     # cross()
     # triangle()
     # clover()
-    vee()
-    steps()
-    u_shape()
-
+    # vee()
+    # steps()
+    # u_shape()
+    # line()
+    # heart()
+    # star()
+    # w_shape()
+    # broken_v()
+    # infinity()
+    pass
 
 
 if __name__ == "__main__":
